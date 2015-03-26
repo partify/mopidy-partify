19a8b6da2899a7fa26e6aac6ba0076a4a55d6357
b3ngr33ni3r <ben@bengreenier.com>
2015-03-25 22:11:00 -0400

    v0.0.5 think i found the problem, damn manifest.in

--- mopidy_partify/__init__.py
+++ mopidy_partify/__init__.py
@@ -13,7 +13,7 @@ import tornado.web
 import tornado.websocket
 
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 __static_path__ = 'static'
 __config_path__ = 'ext.conf'
 
@@ -85,3 +85,4 @@ class PartifyExtension(ext.Extension):
             'name': self.ext_name,
             'path': os.path.join(os.path.dirname(__file__), __static_path__),
         })
+        logger.info("Registered partify as http:static")
